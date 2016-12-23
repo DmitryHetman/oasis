d090b8db059c8a452a5489862413ef1d9302dca8
Michael Forney <mforney@mforney.org>
2016-12-23 15:32:20 -0800

    Add netsurf 3.6

---
+++ pkg/netsurf/config.h
@@ -0,0 +1,16 @@
+#define nsframebuffer
+#define NETSURF_HOMEPAGE "about:welcome"
+#define NETSURF_FB_RESPATH "${HOME}/.netsurf:${NETSURFRES}:/share/netsurf"
+#define NETSURF_FB_FONTPATH "/share/fonts"
+
+#define FB_USE_FREETYPE
+#define NETSURF_FB_FONT_SANS_SERIF "source-sans-pro/SourceSansPro-Regular.otf"
+#define NETSURF_FB_FONT_SANS_SERIF_BOLD "source-sans-pro/SourceSansPro-Bold.otf"
+#define NETSURF_FB_FONT_SANS_SERIF_ITALIC "source-sans-pro/SourceSansPro-It.otf"
+#define NETSURF_FB_FONT_SANS_SERIF_ITALIC_BOLD "source-sans-pro/SourceSansPro-BoldIt.otf"
+#define NETSURF_FB_FONT_SERIF "source-serif-pro/SourceSerifPro-Regular.otf"
+#define NETSURF_FB_FONT_SERIF_BOLD "source-serif-pro/SourceSerifPro-Bold.ttf"
+#define NETSURF_FB_FONT_MONOSPACE "source-code-pro/SourceCodePro-Regular.ttf"
+#define NETSURF_FB_FONT_MONOSPACE_BOLD "source-code-pro/SourceCodePro-Bold.ttf"
+#define NETSURF_FB_FONT_CURSIVE ""
+#define NETSURF_FB_FONT_FANTASY ""
